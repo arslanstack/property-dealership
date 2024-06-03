3c4346f0918cccb02fec841f84d06d56012168de
Muhammad Arslan <being.arslan@outlook.com>
2024-06-03 17:15:54 +0500

    feature/Features Model and migration

--- database/migrations/2024_06_03_113828_create_features_table.php
+++ database/migrations/2024_06_03_113828_create_features_table.php
@@ -11,15 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('neighborhoods', function (Blueprint $table) {
+        Schema::create('features', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->string('slug');
-            $table->string('banner')->nullable();
-            $table->string('zip')->nullable();
-            $table->string('map')->nullable();
-            $table->string('images')->nullable();
-            $table->mediumText('description')->nullable();
+            $table->tinyInteger('type')->comment('1: Interior Feature, 2: Exterior Finish, 3: Featured Amenities, 4: Appliances, 5: Views, 6: Heating, 7: Cooling, 8: Roof, 9: Sewer-Water Systems, 10: Extra Features');
             $table->tinyInteger('status')->default(1)->comment('0: Inactive, 1: Active');
             $table->timestamps();
         });
@@ -30,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('neighborhoods');
+        Schema::dropIfExists('features');
     }
 };
