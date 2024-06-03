4d88365c524be6fb9d9ef96d367996394da3e350
Muhammad Arslan <being.arslan@outlook.com>
2024-06-03 16:37:21 +0500

    feature/Neighborhood Model & Migration

---
+++ app/Models/Neighborhood.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Neighborhood extends Model
+{
+    use HasFactory;
+
+    protected $table = 'neighborhoods';
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'banner',
+        'zip',
+        'map',
+        'images',
+        'description',
+        'status',
+    ];
+
+    public function getBannerAttribute($value)
+    {
+        return $value ? asset('uploads/neighborhoods/' . $value) : null;
+    }
+
+    public function getImagesAttribute($value)
+    {
+        return $value ? json_decode($value) : null;
+    }
+
+    public function setImagesAttribute($value)
+    {
+        $this->attributes['images'] = $value ? json_encode($value) : null;
+    }
+}
---
+++ database/migrations/2024_06_03_112734_create_neighborhoods_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('neighborhoods', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('banner');
+            $table->string('zip');
+            $table->string('map');
+            $table->string('images');
+            $table->mediumText('description');
+            $table->tinyInteger('status')->default(1)->comment('0: Inactive, 1: Active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('neighborhoods');
+    }
+};
