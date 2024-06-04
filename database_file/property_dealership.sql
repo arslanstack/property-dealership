82f62e5099903912d557c902b6ab984fb20719db
Muhammad Arslan <being.arslan@outlook.com>
2024-06-04 14:47:27 +0500

    fix/updated SQL file with logo and Project name

--- database_file/property_dealership.sql
+++ database_file/property_dealership.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 03, 2024 at 03:14 PM
+-- Generation Time: Jun 04, 2024 at 11:46 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -63,6 +63,18 @@ CREATE TABLE `features` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `features`
+--
+
+INSERT INTO `features` (`id`, `title`, `slug`, `type`, `status`, `created_at`, `updated_at`) VALUES
+(2, 'Alarm System', 'alarm-system', 1, 1, '2024-06-04 02:47:27', '2024-06-04 02:47:27'),
+(3, 'Breakfast Nook', 'breakfast-nook', 1, 1, '2024-06-04 02:49:09', '2024-06-04 02:49:09'),
+(4, 'Brick', 'brick', 2, 1, '2024-06-04 02:49:25', '2024-06-04 02:49:25'),
+(5, 'Stucco', 'stucco', 2, 1, '2024-06-04 02:49:37', '2024-06-04 02:49:37'),
+(6, 'Laundry Room', 'laundry-room', 1, 1, '2024-06-04 02:49:51', '2024-06-04 02:49:51'),
+(7, 'Landscaped', 'landscaped', 3, 1, '2024-06-04 02:50:01', '2024-06-04 03:12:45');
+
 -- --------------------------------------------------------
 
 --
@@ -83,7 +95,10 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2019_12_14_000001_create_personal_access_tokens_table', 1),
 (2, '2024_06_03_112734_create_neighborhoods_table', 1),
 (3, '2024_06_03_113828_create_features_table', 2),
-(5, '2024_06_03_122537_create_types_table', 3);
+(5, '2024_06_03_122537_create_types_table', 3),
+(6, '2024_06_03_122834_create_properties_table', 4),
+(7, '2024_06_04_053629_create_property_features_table', 5),
+(8, '2024_06_04_053907_create_property_types_table', 6);
 
 -- --------------------------------------------------------
 
@@ -127,6 +142,71 @@ CREATE TABLE `personal_access_tokens` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `properties`
+--
+
+CREATE TABLE `properties` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `price` varchar(255) NOT NULL,
+  `neighborhood_id` bigint(20) UNSIGNED NOT NULL,
+  `listing_status` tinyint(4) NOT NULL COMMENT '1: For Sale, 2: For Rent, 3: Rented, 4: Sale Pending, 5: Sold',
+  `size` varchar(255) NOT NULL,
+  `bedrooms` int(11) NOT NULL,
+  `bathrooms` int(11) NOT NULL,
+  `parking_spaces` int(11) NOT NULL,
+  `banner` varchar(255) NOT NULL,
+  `gallery` varchar(255) NOT NULL,
+  `map` varchar(255) NOT NULL,
+  `description` mediumtext NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `dev_lvl` tinyint(4) NOT NULL COMMENT '1: Under Construction, 2: Built, 3: Under Renovation, 4: Renovated',
+  `year_built` varchar(255) NOT NULL,
+  `property_tax` varchar(255) NOT NULL,
+  `hoa_fees` varchar(255) NOT NULL,
+  `rent_cycle` tinyint(4) NOT NULL COMMENT '1: Monthly, 2: Quarterly, 3: Semi-Annually, 4: Annually',
+  `date_available` datetime NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 1 COMMENT '0: Inactive, 1: Active',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `property_features`
+--
+
+CREATE TABLE `property_features` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `property_id` bigint(20) UNSIGNED NOT NULL,
+  `feature_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `property_types`
+--
+
+CREATE TABLE `property_types` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `property_id` bigint(20) UNSIGNED NOT NULL,
+  `type_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `settings`
 --
 
@@ -142,9 +222,9 @@ CREATE TABLE `settings` (
 --
 
 INSERT INTO `settings` (`id`, `meta_tag`, `meta_key`, `meta_value`) VALUES
-(1, 'project', 'site_title', 'Property Dealership'),
-(2, 'project', 'short_site_title', 'PD'),
-(3, 'project', 'site_logo', 'logo.svg');
+(1, 'project', 'site_title', 'My Baja Property'),
+(2, 'project', 'short_site_title', 'MBP'),
+(3, 'project', 'site_logo', 'logo.png');
 
 -- --------------------------------------------------------
 
@@ -160,6 +240,16 @@ CREATE TABLE `types` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `types`
+--
+
+INSERT INTO `types` (`id`, `title`, `slug`, `status`, `created_at`, `updated_at`) VALUES
+(2, 'Condo', 'condo', 1, '2024-06-04 04:09:15', '2024-06-04 04:09:15'),
+(3, 'House', 'house', 1, '2024-06-04 04:10:05', '2024-06-04 04:10:05'),
+(4, 'Residential', 'residential', 1, '2024-06-04 04:10:18', '2024-06-04 04:10:18'),
+(5, 'Commercial', 'commercial', 1, '2024-06-04 04:10:29', '2024-06-04 04:10:29');
 
 -- --------------------------------------------------------
 
@@ -225,6 +315,30 @@ ALTER TABLE `personal_access_tokens`
   ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
 
 --
+-- Indexes for table `properties`
+--
+ALTER TABLE `properties`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `properties_code_unique` (`code`),
+  ADD KEY `properties_neighborhood_id_foreign` (`neighborhood_id`);
+
+--
+-- Indexes for table `property_features`
+--
+ALTER TABLE `property_features`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `property_features_property_id_foreign` (`property_id`),
+  ADD KEY `property_features_feature_id_foreign` (`feature_id`);
+
+--
+-- Indexes for table `property_types`
+--
+ALTER TABLE `property_types`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `property_types_property_id_foreign` (`property_id`),
+  ADD KEY `property_types_type_id_foreign` (`type_id`);
+
+--
 -- Indexes for table `settings`
 --
 ALTER TABLE `settings`
@@ -256,13 +370,13 @@ ALTER TABLE `admin_users`
 -- AUTO_INCREMENT for table `features`
 --
 ALTER TABLE `features`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `neighborhoods`
@@ -277,6 +391,24 @@ ALTER TABLE `personal_access_tokens`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `properties`
+--
+ALTER TABLE `properties`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `property_features`
+--
+ALTER TABLE `property_features`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `property_types`
+--
+ALTER TABLE `property_types`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `settings`
 --
 ALTER TABLE `settings`
@@ -286,13 +418,37 @@ ALTER TABLE `settings`
 -- AUTO_INCREMENT for table `types`
 --
 ALTER TABLE `types`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=97;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `properties`
+--
+ALTER TABLE `properties`
+  ADD CONSTRAINT `properties_neighborhood_id_foreign` FOREIGN KEY (`neighborhood_id`) REFERENCES `neighborhoods` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `property_features`
+--
+ALTER TABLE `property_features`
+  ADD CONSTRAINT `property_features_feature_id_foreign` FOREIGN KEY (`feature_id`) REFERENCES `features` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `property_features_property_id_foreign` FOREIGN KEY (`property_id`) REFERENCES `properties` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `property_types`
+--
+ALTER TABLE `property_types`
+  ADD CONSTRAINT `property_types_property_id_foreign` FOREIGN KEY (`property_id`) REFERENCES `properties` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `property_types_type_id_foreign` FOREIGN KEY (`type_id`) REFERENCES `types` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
